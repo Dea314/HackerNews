ef1c55631ab0b51f76288faa0091aee0715223c3
krupabahulya <krupabahulya.y@gmail.com>
2022-03-02 13:46:23 +0100

    boilet plate

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hn.algolia.com/api/v1/search?query=react")
+      .then((res) => res.json())
+      .then((data) => setNews(data.hits))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hacker news</h1>
+      <ul>
+        {news.map((story) => (
+          <li key={story.objectID}>{story.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
