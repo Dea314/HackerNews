ff06381694b9d0df86fcece5240b852e246b3979
Dea314 <fabrisdanijela@gmail.com>
2022-03-07 18:32:26 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import { useState, useEffect } from "react";
+import React from "react";
+import "./styles.css";
+import Form from "./components/Form";
+import axios from "axios";
 
 const App = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [news, setNews] = useState([]);
+  // Query with initial value
+  const [query, setQuery] = useState("react");
 
   useEffect(() => {
-    fetch("https://hn.algolia.com/api/v1/search?query=react")
-      .then((res) => res.json())
-      .then((data) => setNews(data.hits))
-      .catch((err) => console.log(err));
-  }, []);
+    const basicAPI = `https://hn.algolia.com/api/v1/search?query=${query}`;
+    axios.get(basicAPI).then(
+      (res) => {
+        setIsLoaded(true);
+        setNews(res.data.hits);
+      },
+      (error) => {
+        setIsLoaded(false);
+        setError(error);
+        console.error(error);
+      }
+    );
+    // the effect depends on the query
+  }, [query]);
 
-  return (
-    <div className="App">
-      <h1>Hacker news</h1>
-      <ul>
-        {news.map((story) => (
-          <li key={story.objectID}>{story.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  console.log("news", news);
+  if (error) {
+    return <>Error: {error.message}</>;
+  } else if (!isLoaded) {
+    return <>Loading...</>;
+  } else {
+    return (
+      <div className="App">
+        <h1 className="heading">Hacker News</h1>
+        <Form setQuery={setQuery} />
+        <ul>
+          {news.map((story) => (
+            <li key={story.objectID}>
+              {story.title}
+              <br />
+              <span>By: {story.author}</span>
+              <br />
+              <span>Points: {story.points}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default App;
