3ac02a6db1b2aca5c0e48de92839921949628212
Dea314 <93204809+Dea314@users.noreply.github.com>
2022-03-07 21:05:43 +0100

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,14 +2,32 @@ import { useState } from "react";
 import React from "react";
 
 
+const Form = ({ setQuery }) => {
   const [story, setStory] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    
+    if (!story) return alert("Serach cannot be empty");
+   
+    setQuery(story);
+    setStory("");
+  };
 
+  return (
+    <form onSubmit={handleSubmit} method="POST">
+      <label>
+        <input
+          placeholder="Search..."
           type="text"
           value={story}
           onChange={(e) => setStory(e.target.value)}
           key={story.objectID}
         />
+      </label>
 
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+export default Form;
